47ab50acedfb7107e05902bfe492037dfc88109b
junxianzzz97 <73930174+junxianzzz97@users.noreply.github.com>
2021-06-30 04:10:28 +0800

    Add files via upload

---
+++ q35.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//算階乘
+using namespace std;
+
+int main()
+{
+    int n,i;
+    //int型態會溢位
+    long sum=1;
+    cin >> n;
+    for(i=1; i<=n; i++)
+        sum*=i;
+    cout << sum << endl;
+
+    return 0;
+}
